9ab61f104b3a659e4702437fcf25df495a5ac75e
Roman Strohan <r.strogan@pnn.in.ua>
2021-05-17 14:38:29 +0300

    add native js pop with styles

---
+++ main.js
@@ -0,0 +1,16 @@
+const showBtnEl = document.getElementById("showBtn");
+const closeBtnEl = document.getElementById("closeBtn");
+const popupEl = document.getElementById("popup");
+
+showBtnEl.addEventListener("click", () => {
+  togglePopup("hide", "show");
+});
+
+closeBtnEl.addEventListener("click", () => {
+  togglePopup("show", "hide");
+});
+
+function togglePopup(removeClassName, addClassName) {
+  popupEl.classList.remove(removeClassName);
+  popupEl.classList.add(addClassName);
+}
